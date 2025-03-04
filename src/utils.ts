2edba0a136f55f6f676c29b608ffa12a2aab6309
Angel Dijoux <angel.dijoux@yahoo.com>
2025-03-04 20:49:29 +0100

    fix: create aqora config directory

--- src/utils.ts
+++ src/utils.ts
@@ -2,14 +2,16 @@ type CamelToSnakeCase<S extends string> = S extends `${infer T}${infer U}`
   ? `${T extends Capitalize<T> ? "_" : ""}${Lowercase<T>}${CamelToSnakeCase<U>}`
   : S;
 
-export type CamelToSnakeCaseNested<T> = T extends object ? {
-    [K in keyof T as CamelToSnakeCase<K & string>]: CamelToSnakeCaseNested<
-      T[K]
-    >;
-  }
+export type CamelToSnakeCaseNested<T> = T extends object
+  ? {
+      [K in keyof T as CamelToSnakeCase<K & string>]: CamelToSnakeCaseNested<
+        T[K]
+      >;
+    }
   : T;
 
-type SnakeToCamelCase<S extends string> = S extends `${infer T}_${infer U}` ? `${T}${Capitalize<SnakeToCamelCase<U>>}`
+type SnakeToCamelCase<S extends string> = S extends `${infer T}_${infer U}`
+  ? `${T}${Capitalize<SnakeToCamelCase<U>>}`
   : S;
 
 export type SnakeToCamelCaseNested<T extends Record<string, any>> = {
